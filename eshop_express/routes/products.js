08bc08f43dd10f8e398dbbe5fa370d3da7dee59e
robbanimustofa <robbanimustofa@gmail.com>
2022-11-12 23:08:46 +0700

    Delete Product

--- eshop_express/routes/products.js
+++ eshop_express/routes/products.js
@@ -84,4 +84,17 @@ router.put('/:id', async(req, res)=>{
     
 })
 
+// API Router DELETE Product
+router.delete('/:id', (req,res)=>{
+    Product.findByIdAndRemove(req.params.id).then(product =>{
+        if(product){
+            return res.status(200).json({success: true, message: 'Berhasil Menghapus Product'})
+        }else{
+            return res.status(404).json({success:false, message: 'Product Not Found'})
+        }
+    }).catch(err=>{
+        return res.status(400).json({success: false, error: err, message:err.message})
+    })
+})
+
 module.exports = router;
