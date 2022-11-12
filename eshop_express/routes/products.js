b5e72fa5eae037f7d1ab63b69c2c9a68bea876c6
robbanimustofa <robbanimustofa@gmail.com>
2022-11-12 23:04:40 +0700

    Update and Detail Product

--- eshop_express/routes/products.js
+++ eshop_express/routes/products.js
@@ -3,25 +3,26 @@ const express = require('express');
 const { Category } = require('../models/category.js');
 const router = express.Router();
 
+// API Router GET All Product
 router.get(`/`,async (req, res)=>{
     const productList = await Product.find().populate('category');
     if(!productList){
         res.status(500).json({success: false})
     }
     res.send(productList)
-    // res.send('Test Products')
 })
 
 // API Router GET Detail Product
 router.get('/:id', async(req,res)=>{
-    const detailCategory = await Product.findById(req.params.id);
+    const detailProduct = await Product.findById(req.params.id);
 
-    if(!detailCategory){
-        res.status(500).json({message:'Detail Category Tidak Ditemukan karna ID category salah'})
+    if(!detailProduct){
+        res.status(500).json({message:'Detail Product Tidak Ditemukan karna ID product salah'})
     }
-    res.status(200).send(detailCategory)
+    res.status(200).send(detailProduct)
 })
 
+// API Router POST Product
 router.post(`/`,async (req,res)=>{    
     try {
         const category = await Category.findById(req.body.category)
@@ -41,15 +42,46 @@ router.post(`/`,async (req,res)=>{
         })
         const data = product
         await data.save()
-        if(!data)
-        return res.status(500).send("Product Cannot be Created")
+        if(!data){
+            return res.status(500).send("Product Cannot be Created")
+        }
         res.status(200).json({data, message:'Berhasil Menyimpan Data', status:200})
-        // res.send(product)
-        // await product.save()
-        // res.status(200).json({product, message:'Berhasil Menyimpan Data', status:200})
     } catch (error) {
         res.status(400).json({message:error.message, Status:400})
     }
+})
+
+// API Update Product
+router.put('/:id', async(req, res)=>{
+    try {
+    const category = await Category.findById(req.body.category)
+    if(!category){
+        return res.status(400).send('Invalid Category')
+    } 
+    const updateProduct = await Product.findByIdAndUpdate(
+        req.params.id,{
+            name:req.body.name,
+            description:req.body.description,
+            richDescription:req.body.richDescription,
+            image:req.body.image,
+            brand:req.body.brand,
+            price:req.body.price,
+            category:req.body.category,
+            countInStock:req.body.countInStock,
+            rating:req.body.rating,
+            numReviews:req.body.numReviews,
+            isFeatured:req.body.isFeatured
+        },{new:true}
+    )
+
+    if(!updateProduct){
+        res.status(500).json({success: false,message:"Product Cannot be Update"})
+    }
+    res.send(updateProduct)
+    } catch (error) {
+        res.status(400).json({message:error.message, Status:400})
+    }
+    
 })
 
 module.exports = router;
