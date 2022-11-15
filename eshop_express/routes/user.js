d40c65f25fb8f6a5ed3a4d0728e07737df57f35c
robbanimustofa <robbanimustofa@gmail.com>
2022-11-15 13:56:30 +0700

    Install Jsonwebtoken, post login user with token

--- eshop_express/routes/user.js
+++ eshop_express/routes/user.js
@@ -6,7 +6,8 @@ const express = require('express');
 const router = express.Router();
 // import bcrypt
 const bcrypt = require('bcryptjs')
-
+// import jsonwebtoken
+const jwt = require('jsonwebtoken')
 
 // API Router GET All Users
 router.get(`/`,async (req, res)=>{
@@ -52,12 +53,35 @@ router.post('/',async (req,res)=>{
         const data = user
         await data.save()
         if(!data){
-            return res.status(500).send("User Cannot be Created")
+            res.status(500).json({success: false, status:500, message:`Terdapat Input Yang Kosong!`})
         }
-        res.status(200).json({data, message:'Berhasil Menyimpan Data', status:200})
+        res.status(200).json({data, message:'Berhasil Membuat User Baru!', status:200})
+    } catch (error) {
+        res.status(400).json({success: false, status:400, message:`Gagal Membuat User Baru (Name and Email already exist)`})
+    }
+})
+
+// API Router GET Users Login
+router.post(`/login`,async (req, res)=>{
+    try {
+        const data = await User.findOne({email: req.body.email})
+        if(!data){
+            res.status(500).json({success: false, message:"User Tidak Ditemukan atau belum Terdaftar!"})
+        }
+        if(data && bcrypt.compareSync(req.body.password, data.passwordHash)){
+            const token = jwt.sign({
+                userId: data._id
+            }, 'secret',{expiresIn:'1d'}
+            )
+            res.status(200).json({data: data.email, token:token})
+        }else{
+            res.status(500).json({success: false, message:"Password salah!"})
+        }
     } catch (error) {
         res.status(400).json({message:error.message, Status:400})
     }
 })
+
+
 
 module.exports = router;
